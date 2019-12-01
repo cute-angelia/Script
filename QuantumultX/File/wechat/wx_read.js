fe983274172b0f54c887d2f5d1ab07e68f9f634d
Vanilla <rose20.99.c@gmail.com>
2019-12-01 21:17:18 +0800

    [update]

---
+++ QuantumultX/File/wechat/wx_read.js
@@ -0,0 +1,29 @@
+/*
+微信阅读
+
+i.weread.qq.com
+
+@supported E54765DF5F47
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/pay/memberCardSummary';
+const free = '/book/info';
+
+if (url.indexOf(vip) != -1) {
+    obj.data["expiredTime"] = 1890623940;
+    obj.data["remainTime"] = 354045670;
+    obj.data["freeBookIds"] = ["25444718", "25445167"];
+    body = JSON.stringify(obj);
+}
+
+if (url.indexOf(free) != -1) {
+    obj.data["maxFreeChapter"] = 2000;
+    obj.data["free"] = 1;
+    body = JSON.stringify(obj);
+}
+
+$done({ body });
