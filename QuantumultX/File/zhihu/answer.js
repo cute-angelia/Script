741b3973f022058f1ed9ccc64b04eef4a4c214dc
Vanilla <rose20.99.c@gmail.com>
2019-12-02 16:32:30 +0800

    [update]

---
+++ QuantumultX/File/zhihu/answer.js
@@ -0,0 +1,22 @@
+/**
+ * @name 知乎回答
+ * @supported E54765DF5F47
+ */
+
+//regex: ^https://api.zhihu.com/.*/questions
+//Original author: onewayticket255
+
+let body = $response.body
+body = JSON.parse(body)
+if (body['ad_info']) {
+    delete body['ad_info']
+};
+if (body.data) {
+    body['data'].forEach((element, index) => {
+        if (element['author']['name'] == "盐选推荐") {
+            body['data'].splice(index, 1)
+        }
+    })
+};
+body = JSON.stringify(body)
+$done({ body })
---
+++ QuantumultX/File/zhihu/market.js
@@ -0,0 +1,15 @@
+/**
+ * @name Remove Zhihu ads
+ * @Regex: ^https://api.zhihu.com/market/header
+ * @supported E54765DF5F47
+ */
+
+
+let body = $response.body
+body = JSON.parse(body)
+body['sub_webs'].splice(0, 1)
+body['sub_webs'].splice(1, 1)
+body = JSON.stringify(body)
+$done({ body })
+
+// by onewayticket255
