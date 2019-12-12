3dad472278c8c039ad647ec69f3b2952895e1cfd
Vanilla <rose20.99.c@gmail.com>
2019-12-12 18:45:11 +0800

    [update]

--- QuantumultX/File/raindrop/index.js
+++ QuantumultX/File/raindrop/index.js
@@ -9,6 +9,8 @@ var url = $request.url;
 var obj = JSON.parse(body);
 
 const userConfig = '/v1/userConfig';
+const stat = '/v1/stat';
+const user = '/v1/user';
 
 if (url.indexOf(vip) != -1) {
     if (obj.hasOwnProperty('user')) {
@@ -19,6 +21,24 @@ if (url.indexOf(vip) != -1) {
     if (obj.hasOwnProperty('item')) {
         if (obj.item.hasOwnProperty('pro')) {
             obj.item.pro = true;
+        }
+    }
+    body = JSON.stringify(obj);
+}
+
+if (url.indexOf(stat) != -1) {
+    if (obj.hasOwnProperty('meta')) {
+        if (obj.meta.hasOwnProperty('pro')) {
+            obj.meta.pro = true;
+        }
+    }
+    body = JSON.stringify(obj);
+}
+
+if (url.indexOf(user) != -1) {
+    if (obj.hasOwnProperty('user')) {
+        if (obj.user.hasOwnProperty('pro')) {
+            obj.user.pro = true;
         }
     }
     body = JSON.stringify(obj);
